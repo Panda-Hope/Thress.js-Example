6ce66865fc26a37a1b3acbeab9a5fd8d7b5f0e98
hope <494873674@qq.com>
2022-08-31 08:14:42 +0800

    feat: 🎸 updaet electron config

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log(`欢迎来到 Electron 👋`)
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 1000,
+  })
+
+  // node static server
+  win.loadURL('https://woaixiaoqianqian.cn/nft/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: process.cwd(),
+  base: process.env.NODE_ENV === 'production' ? '/nft/' : './',
   plugins: [vue()]
 })
